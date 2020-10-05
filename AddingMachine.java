9356ff009e3483a622a396a9c389d1baa6768425
stephanietripodi63 <70589856+stephanietripodi63@users.noreply.github.com>
2020-10-05 16:51:52 -0700

    Add files via upload
    second file

--- AddingMachine.java
+++ AddingMachine.java
@@ -6,25 +6,32 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  StringBuffer history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = new StringBuffer("0";)
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
-  public void add (int value) {
+  public void add (int value) { //add parameter from total
+	  total = total+value;
+	  history.append("+"+value);
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) { //subtract parameter from total
+	  total = total-value;
+	  history.append("-"+value);
   }
 
   public String toString () {
-    return "";
+    return history.toString();
   }
 
-  public void clear() {
+  public void clear() { //clears memory
+	  return 0;
   }
 }
